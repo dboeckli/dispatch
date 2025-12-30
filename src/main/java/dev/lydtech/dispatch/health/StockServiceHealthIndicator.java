e8656a937ce90a48b65099bd0ff10fb99f932f09
Dominique Boeckli <dboeckli@gmail.com>
2025-12-30 09:52:08 +0100

    fixup! fix spring boot 4 upgrade

--- src/main/java/dev/lydtech/dispatch/health/StockServiceHealthIndicator.java
+++ src/main/java/dev/lydtech/dispatch/health/StockServiceHealthIndicator.java
@@ -3,8 +3,8 @@ package dev.lydtech.dispatch.health;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.health.contributor.Health;
+import org.springframework.boot.health.contributor.HealthIndicator;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
